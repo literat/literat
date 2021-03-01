da1cbb32f252b1c188b8186878e2f7ee4ac5be76
literat <tomas@litera.me>
2021-03-01 09:06:33 +0100

    feat: Display header title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,46 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+const TitleAndDescription = ({ data }) => {
+  const { title, description } = data.site.siteMetadata
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: "avenir",
+      }}
+    >
+      <h2 style={{ marginBottom: 0 }}>{title}</h2>
+      <p style={{ marginTop: 0, opacity: 0.5 }}>{description}</p>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              description
+            }
+          }
+        }
+      `}
+      render={data => <TitleAndDescription data={data} />}
+    />
+  )
+}
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
