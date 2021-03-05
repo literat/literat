d83ada9c86266bbf3eb067ddb11346799cea8f67
literat <tomas@litera.me>
2021-03-05 13:42:12 +0100

    feat: Display post content

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,31 @@
 import React from "react"
 
-const Template = props => {
-  return <div>Blog Post Here</div>
+const Template = ({ data }) => {
+  const { markdownRemark } = data
+  const { html } = markdownRemark
+  const { title } = markdownRemark.frontmatter
+
+  return (
+    <div>
+      <h1 style={{ fontFamily: "avenir" }}>{title}</h1>
+      <div
+        className="blogpost"
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={{ fontFamily: "avenir" }}
+      ></div>
+    </div>
+  )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default Template
