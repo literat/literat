6e64da9fde0247706cc62552e6cc89e2a9dd5862
literat <tomas@litera.me>
2021-04-28 21:50:07 +0200

    style: Global font, headings, anchors and lists

--- src/components/styles/GlobalStyles.tsx
+++ src/components/styles/GlobalStyles.tsx
@@ -7,6 +7,40 @@ const GlobalStyles = createGlobalStyle`
     src: url(${font}) format('truetype');
     font-style: normal;
   }
+
+  :root {
+    --dark: #000000;
+    --light: #ffffff;
+  }
+
+  html {
+    font-family: 'Fira Code', 'sans-serif';
+    font-size: 10px;
+  }
+
+  body {
+    font-size: 2rem;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: Signika, sans-serif;
+    font-weight: normal;
+    /* font-style: italic; */
+    line-height: 1.5;
+  }
+
+  a {
+    color: var(--dark);
+  }
+
+  p, ul, ol {
+    line-height: 1.77777777777777776777777777;
+    font-weight: 400;
+  }
+
+  ul {
+    list-style-type: square;
+  }
 `;
 
 export default GlobalStyles;
