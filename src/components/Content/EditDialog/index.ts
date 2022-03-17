9f0f2b882b76ec84679fcbf7bdab7913679e0769
literat <tomas@litera.me>
2022-03-17 10:27:10 +0100

    feat: introduce content edit dialog

---
+++ src/components/Content/EditDialog/index.ts
@@ -0,0 +1,3 @@
+export * from './EditDialog';
+export * from './EditDialogStyles';
+export { default as EditDialog } from './EditDialog';
--- src/templates/blogPost.tsx
+++ src/templates/blogPost.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import ContentNav from '../components/ContentNav';
 import H from '../components/mdx/Headings';
+import { EditDialog } from '../components/Content/EditDialog';
 
 interface PageNav {
   fields: {
@@ -14,6 +15,7 @@ interface TemplateProps {
   data: {
     mdx: {
       body: string;
+      fileAbsolutePath: string;
       frontmatter: {
         title: string;
       };
@@ -26,15 +28,18 @@ interface TemplateProps {
 }
 
 const Template = ({ data, pageContext }: TemplateProps) => {
-  const { mdx } = data;
-  const { body } = mdx;
-  const { title } = mdx.frontmatter;
+  const { mdx: post } = data;
+  const { body } = post;
+  const { title } = post.frontmatter;
   const { next, previous } = pageContext;
+
+  const editUrl = `https://github.com/literat/literat/tree/master/src/${post.fileAbsolutePath.split('/src/')[1]}`;
 
   return (
     <div>
       <H>{title}</H>
       <MDXRenderer>{body}</MDXRenderer>
+      <EditDialog editUrl={editUrl} />
       <ContentNav previous={previous} next={next} />
     </div>
   );
@@ -47,6 +52,7 @@ export const query = graphql`
       fields {
         path
       }
+      fileAbsolutePath
       frontmatter {
         title
       }
