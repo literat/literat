cbf5431b9551b316d0936767a94f470f68d1dd37
literat <tomas@litera.me>
2022-04-06 10:46:57 +0200

    fix: not using Fira Sans enywhere

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-import FiraSans from '../assets/fonts/Fira/Sans/FiraSans-Regular.ttf';
 import FiraCode from '../assets/fonts/Fira/Code/FiraCode-Regular.ttf';
 import FiraCodeSemiBold from '../assets/fonts/Fira/Code/FiraCode-SemiBold.ttf';
 import Overpass from '../assets/fonts/Overpass/Overpass-Bold.woff';
@@ -8,14 +7,6 @@ import OverpassMono from '../assets/fonts/OverpassMono/OverpassMono-Regular.ttf'
 import OverpassItalic from '../assets/fonts/Overpass/Overpass-BlackItalic.ttf';
 
 const Fonts = createGlobalStyle`
-  @font-face {
-    font-family: 'Fira Sans';
-    src: url(${FiraSans}) format('truetype');
-    font-weight: 400;
-    font-style: normal;
-    font-display: swap;
-  }
-
   @font-face {
     font-family: 'Fira Code';
     src: url(${FiraCode}) format('truetype');
