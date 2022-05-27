5bbf97359fb32943a7d1b2c0723c468ee4618132
literat <tomas@litera.me>
2022-05-27 20:59:14 +0200

    test: install packages which is no longer shipped with Jest
      * jest-environment-jsdom
      * remove workaround from jest config which is fixed in Jest 28

--- config/jest/config.js
+++ config/jest/config.js
@@ -20,7 +20,6 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/__mocks__/file-mock.js',
-    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
   },
 
   // An array of regexp pattern strings that are matched against all test paths before executing the test
