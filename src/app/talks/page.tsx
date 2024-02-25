215bc1f9a52c68d908ca0e9b65783e263ddedbe2
literat <tomas@litera.me>
2024-02-25 21:03:01 +0100

    refactor: migrate talks page to next

---
+++ src/app/talks/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Speaking & Training',
+};
+
+export default function TalksPage() {
+  return <div>Speaking and training</div>;
+}
