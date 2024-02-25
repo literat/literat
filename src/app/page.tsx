b3a3de4a346fe41baba700d2de953448232befac
literat <tomas@litera.me>
2024-02-25 21:00:45 +0100

    refactor: migrate homepage to next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Page</div>;
+import { fira } from '@/ui/fonts';
+import { Metadata } from 'next';
+import H from '../components/mdx/Headings';
+
+export const metadata: Metadata = {
+  title: 'Fullstack Developer & Whitewater Kayaker & Scout',
+};
+
+export default function HomePage() {
+  return (
+    <div>
+      <div
+        className={fira.className}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <H as="h2">Hi, I am Literat.</H>
+        <p>The Fullstack Developer from Czech republic 🇨🇿</p>
+      </div>
+    </div>
+  );
 }
