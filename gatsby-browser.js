6135428ab3e5d19926243281e5e303c1fcd2e9a4
literat <tomas@litera.me>
2024-03-08 17:19:22 +0100

    refactor: move gatsby browser from cjs to esm

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,10 @@
-const React = require('react');
-const Layout = require('./src/components/Layout').default;
+import React from 'react';
+import Layout from './src/components/Layout';
 
-exports.wrapPageElement = ({ element, props }) => (
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  <Layout {...props}>{element}</Layout>
-);
+export function wrapPageElement({ element, props }) {
+  return (
+    // props provide same data to Layout as Page element will get
+    // including location, data, etc - you don't need to pass it
+    <Layout {...props}>{element}</Layout>
+  );
+}
