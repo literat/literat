ed512fde40a5d6263964e57fba293d486c07b895
literat <tomas@litera.me>
2024-03-27 14:07:22 +0100

    fixup! style: enable linting of the mdx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   rules: {
     'react/jsx-uses-react': 0,
     'react/react-in-jsx-scope': 0,
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'] }],
     'react/jsx-props-no-spreading': 'off',
     'prettier/prettier': [
       'error',
