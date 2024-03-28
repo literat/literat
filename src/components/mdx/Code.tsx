a5ba130f62c54b09ce847e32de3a75daf2d4e0bc
literat <tomas@litera.me>
2024-03-28 18:16:09 +0100

    fix: styling of the syntax highlighting

--- src/components/mdx/Code.tsx
+++ src/components/mdx/Code.tsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 const Code = styled.code`
-  margin-left: auto;
+  /* margin-left: auto;
   margin-right: auto;
   padding-left: 1.2em;
-  padding-right: 1.2em;
+  padding-right: 1.2em; */
+
+  background: #f9f7fb;
+  border: 1px solid #ede7f3;
+  border-radius: 4px;
+  padding: 2px 6px;
+  font-size: 0.9375em;
+  margin-left: auto;
+  margin-right: auto;
 
   &:not(.grvsc-code) {
     background-color: transparent;
