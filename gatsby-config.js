35f988074ed06ca6e6e7dcd7984a90b4ab7c0f53
literat <tomas@litera.me>
2024-05-08 22:27:10 +0200

    chore: enable scss modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
