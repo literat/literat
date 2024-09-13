7dd5a0fbd37fa0b58d502606021237540660d533
literat <tomas@litera.me>
2024-09-13 15:40:04 +0200

    style: introduce textlint for linting the posts content

---
+++ .textlintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    '@lmc-eu/textlint-rule-preset-lmc': {
+      'title-case': {
+        headingLevels: [6],
+      },
+      'stop-words': {
+        severity: 'warning',
+      },
+    },
+  },
+};
